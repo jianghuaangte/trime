72552ebcccf6caac19d92ac2bb00abeaa2d215fb
Your Name <you@example.com>
2025-08-23 10:08:52 +0800

    update

--- app/src/main/java/com/osfans/trime/data/base/DataManager.kt
+++ app/src/main/java/com/osfans/trime/data/base/DataManager.kt
@@ -111,9 +111,14 @@ object DataManager {
             DataDiff.diff(oldChecksums, newChecksums).sortedByDescending { it.ordinal }.forEach {
                 Timber.d("Diff: $it")
                 when (it) {
+//                    is DataDiff.CreateFile,
+//                    is DataDiff.UpdateFile,
+//                    -> ResourceUtils.copyFile(it.path, sharedDataDir, "rime/")
                     is DataDiff.CreateFile,
-                    is DataDiff.UpdateFile,
-                    -> ResourceUtils.copyFile(it.path, sharedDataDir, "rime/")
+                    is DataDiff.UpdateFile -> {
+                        val fixedPath = it.path.removePrefix("shared/")
+                        ResourceUtils.copyFile(fixedPath, sharedDataDir, "rime/")
+                    }
                     is DataDiff.DeleteDir,
                     is DataDiff.DeleteFile,
                     -> FileUtils.delete(sharedDataDir.resolve(it.path.removePrefix("rime/"))).getOrThrow()
