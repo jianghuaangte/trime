f46e62ce2eabeac1663f82e82ccc642007a79873
Your Name <you@example.com>
2025-08-23 11:24:47 +0800

    update

--- app/src/main/java/com/osfans/trime/data/base/DataManager.kt
+++ app/src/main/java/com/osfans/trime/data/base/DataManager.kt
@@ -113,7 +113,7 @@ object DataManager {
                 when (it) {
                     is DataDiff.CreateFile,
                     is DataDiff.UpdateFile,
-                    -> ResourceUtils.copyFile(it.path, sharedDataDir, "")
+                    -> ResourceUtils.copyFile(it.path, sharedDataDir, "rime/")
                     is DataDiff.DeleteDir,
                     is DataDiff.DeleteFile,
                     -> FileUtils.delete(sharedDataDir.resolve(it.path.removePrefix("rime/"))).getOrThrow()
